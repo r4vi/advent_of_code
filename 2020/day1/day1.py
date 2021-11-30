79e950effa4bd7865966125f90c39ac562e58ad2
Ravi Kotecha <kotecha.ravi@gmail.com>
2021-11-30 17:47:17 +0000

    use itertools for 2020/day1

--- 2020/day1/day1.py
+++ 2020/day1/day1.py
@@ -1,16 +1,14 @@
+from itertools import combinations
+
 def main(target):
     input_numbers = []
     with open("./input", "r") as input_file:
         input_values = input_file.readlines()
         input_numbers = [int(x.strip()) for x in input_values]
-    for idx, num_x in enumerate(input_numbers):
-        for cur, num_y in enumerate(input_numbers):
-            for cur_z, num_z in enumerate(input_numbers):
-                if idx == cur == cur_z:
-                    continue
-                if num_x + num_y + num_z == target:
-                    print(f"{num_x} + {num_y} == {target}, therefore {num_x} * {num_y} * {num_z} is {num_x * num_y * num_z}")
-                    break
+    possible_triples = combinations(input_numbers, 3)
+    for num_x, num_y, num_z in possible_triples:
+        if num_x + num_y + num_z == target:
+            print(f"{num_x} + {num_y} == {target}, therefore {num_x} * {num_y} * {num_z} is {num_x * num_y * num_z}")
 
 if __name__ == '__main__':
     TARGET = 2020
