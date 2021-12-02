46e515b66a54033b9a4ff7728831d79d40a4c418
Ravi Kotecha <kotecha.ravi@gmail.com>
2021-12-02 14:21:32 +0000

    clean up day2 part1 file

--- 2021/day2/day2.py
+++ 2021/day2/day2.py
@@ -54,10 +54,6 @@ def part_one(values) -> int:
     return sub.x * sub.y
 
 
-def part_two():
-    pass
-
-
 @pytest.fixture
 def sample():
     return ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]
